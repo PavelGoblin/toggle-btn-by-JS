3019c09bc60c92a90feed5f4f78e06048fb25174
PavelGoblin <pvlbrhm@gmail.com>
2024-10-23 22:14:21 +0600

    Solve toggle issue

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
-let modeBtn= document.querySelector(#mode)
+/*let modeBtn= document.querySelector("#mode")
 let body = document.querySelector("body");
 let currMode="light"
-modeBtn.addEventListner ("click",() =>{
-  if(currMode==="light")
-  {
+  modeBtn.addEventListener("click",()=>{
+    if(currMode==="light")
+      {
+        currMode="dark";
+      body.classList.add ="dark";
+      body.classList.remove ="light"
+      }else{
+        currMode ="dark";
+        body.classList.add="light"
+        body.classList.remove ="dark";
+      }
+      console.log(currMode);
+      
+    });
+
+  */
+ let modeBtn = document.querySelector("#mode");
+ let currMode="light";
+ let body = document.querySelector("body");
+ modeBtn.addEventListener("click",() => {
+  if(currMode==="light"){
     currMode="dark";
-  body.classList.add ="dark";
-  body.classList.remove ="light"
+    //document.querySelector("body").style.backgroundColor ="black";
+    body.classList.add("dark");
+    body.classList.remove("light")
   }else{
-    currMode ="dark";
+    currMode="light";
+    //document.querySelector("body").style.backgroundColor ="white";
     body.classList.add("light");
-    body.classList.remove("dark");
+    body.classList.remove("dark")
   }
   console.log(currMode);
   
-});
+  //console.log("you are trying to change mode");
+  
+ })
